f8b2b751bac2479bc0ad6c6b6ede1a784df7dafc
Elton Ribeiro <eltonribeiro@outlook.com>
2020-12-05 17:18:13 -0300

    feat: migrate to mysql Migrate from sqlLite to MySql Add MySql container to docker-compose Refactory some code

--- backend/go.mod
+++ backend/go.mod
@@ -6,4 +6,7 @@ require (
 	"github.com/gorilla/mux" v1.8.0
 	"github.com/mattn/go-sqlite3" v1.10.0
 	"github.com/sirupsen/logrus" v1.7.0
+	"gorm.io/gorm" v1.20.8
+	"gorm.io/driver/mysql" v1.0.3
+	"github.com/spf13/viper" v1.7.1
 )
---
+++ backend/internal/durable/database.go
@@ -0,0 +1,30 @@
+package durable
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+// ConnectionInfo is the info of the mysql
+type ConnectionInfo struct {
+	Username string
+	Password string
+	Address  string
+	DBName   string
+}
+
+//OpenDatabaseClient opens a connection to the database
+func OpenDatabaseClient(ctx context.Context, c *ConnectionInfo) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?tls=skip-verify&charset=utf8&parseTime=true", c.Username, c.Password, c.Address, c.DBName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return db, nil
+}
