baee64ed32b5dd99b2681e6cbd9fa70ed78ab183
Elton Ribeiro <eltonribeiro@outlook.com>
2020-12-09 01:00:53 -0300

    ta andando

--- frontend/frontend/users/service.py
+++ frontend/frontend/users/service.py
@@ -1,26 +1,29 @@
 import requests, json
 
 def get_all_users(app):
+    content = {}
     backend_url = app.config['BACKEND_URL']+"/users"
     result = requests.get(url=backend_url)
-    if result.status_code == 200:
-        return json.loads("{'status': result.status_code, 'content': result.content}")
-    return json.loads({'status':result.status_code, 'content': result.reason})
+    content["status_code"] = (result.status_code)
+    if result.status_code != 404:
+        content["content"] = (json.loads(result.content))
+    return content
         
 def get_user(app, id:int):
     backend_url = app.config['BACKEND_URL']+"/users/"+id
     return requests.get(url=backend_url)
 
-def delete_user(app, id:int):
+def delete_user(app, id:str):
     backend_url = app.config['BACKEND_URL']+"/users/"+id
     return requests.delete(url=backend_url)
 
 def create_user(app, payload:dict):
+    content = {}
     backend_url = app.config['BACKEND_URL']+"/users"
     result = requests.post(url=backend_url, data=json.dumps(payload))
-    if result.status_code == 201:
-        return json.loads({"status":result.status_code, "content": result.content})
-    return json.loads({"status":result.status_code, "content": result.reason})
+    content["status_code"] = (result.status_code)
+    content["content"] = (json.loads(result.content))
+    return content
 
 def update_user(app, id:int, payload:str):
     backend_url = app.config['BACKEND_URL']+"/users"+id
--- frontend/frontend/users/users.py
+++ frontend/frontend/users/users.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask import current_app as app
 from . import service
 
@@ -9,23 +9,32 @@ users_bp = Blueprint(
     static_folder='static',
 )
 
-@users_bp.route('/users', methods=['GET', 'POST'])
+@users_bp.route('/users', methods=['GET'])
 def users():
-    result = service.get_all_users(app)
-    if result != None:
+    users = service.get_all_users(app)
+    if users != None:
         return render_template(
             'users.html',
-            result=result
+            users=users,
+            new_user=None
         )
-    if request.method == 'POST':
-        user = {}
-        user.update({'full_name': request.form.get('full_name')})
-        user.update({'username': request.form.get('username')})
-        user.update({'email': request.form.get('email')})
 
-        result = service.create_user(app, user)
+@users_bp.route('/users', methods=['POST'])
+def new_user():
+    user = {}
+    user.update({'full_name': request.form.get('full_name')})
+    user.update({'username': request.form.get('username')})
+    user.update({'email': request.form.get('email')})
+
+    new_user = service.create_user(app, user)
     
-    return render_template(
-        'users.html',
-        result=result
-    )
+    return redirect(url_for('users_bp.users', new_user=new_user))
+
+
+@users_bp.route('/users/delete', methods=['POST'])
+def delete_user():
+    user_id = request.form.get('id')
+    
+    user_deleted = service.delete_user(app, user_id)
+
+    return redirect(url_for('users_bp.users'))
